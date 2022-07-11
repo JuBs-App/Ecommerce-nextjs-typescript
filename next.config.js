3d543e60c46e6573045894cc5735968e9cb99cef
jubin369 <jubinayoob369@gmail.com>
2022-07-12 00:07:51 +0530

    Image Loader

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ['rickandmortyapi.com'],
+    loader: 'custom',
+    path: '/',
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 }
 
