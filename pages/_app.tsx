49ecd4b1691702e0ee730c46c5278410b3a82b8a
jubin369 <jubinayoob369@gmail.com>
2022-07-12 08:29:07 +0530

    Adds font style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,8 @@
 import '../styles/globals.css'
+import theme from '../styles/theme'
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
-
-// 2. Extend the theme to include custom colors, fonts, etc
-const colors = {
-  brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
-  },
-}
-
-const theme = extendTheme({ colors })
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
